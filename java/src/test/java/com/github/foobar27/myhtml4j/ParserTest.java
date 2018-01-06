58e4d6d95b82916d3f3582ed13bb168eb25d6eb7
Sébastien Wagener <sebastien.wagener@gmail.com>
2018-01-06 20:48:36 +0100

    Reproduce segfault

--- java/src/test/java/com/github/foobar27/myhtml4j/ParserTest.java
+++ java/src/test/java/com/github/foobar27/myhtml4j/ParserTest.java
@@ -41,7 +41,14 @@ public class ParserTest {
                 {
                         "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Frameset//EN\" \"http://www.w3.org/TR/html4/frameset.dtd\"><html><head><title>Hello world</title></head></body></html>",
                         "Document[Optional[[html, PUBLIC, -//W3C//DTD HTML 4.01 Frameset//EN, http://www.w3.org/TR/html4/frameset.dtd]],Element[html,html,{},[Element[html,head,{},[Element[html,title,{},[Text[Hello world]]]]], Element[html,body,{},[]]]]]"
-                }});
+                },
+                // Reproduce segfault from myhtml issue 124
+                // https://github.com/lexborisov/myhtml/issues/124
+                {
+                        "<!DOCTYPE html><html><body><template><div></div></form></template></body></html>",
+                        "Document[Optional[[html]],Element[html,html,{},[Element[html,head,{},[]], Element[html,body,{},[Element[html,template,{},[Element[html,div,{},[]]]]]]]]]"
+                }
+        });
     }
 
     private final String input;
